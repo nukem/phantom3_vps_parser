efb1c223609d05e09a3bcf785aac24130754b495
Francis V <lugaw@outlook.com>
2019-03-26 13:46:56 +0900

    output formatting

--- parse_output.py
+++ parse_output.py
@@ -7,15 +7,13 @@ ser = serial.Serial("COM3", 115200)
 while True:
 	data = ser.read()
 	if data == b'\x55':
-		data = ser.read()
-		if data == b'\x0b':
-			out = ser.read(3)
+		data = ser.read(4)
+		if data == bytearray(b'\x0B\x00\xF8\x03'):
 			out = ser.read(6)
-			Height, ID, Count, Random = struct.unpack("<HBBh", out)
-			# print(Height)
-		elif data == b'\x57':
-			out = ser.read(5)
-			out = ser.read(6)
-			X, Y, Z = struct.unpack("<hhH", out)
-			print (X, Y)
-
+			Height, Count, Random = struct.unpack("<HHH", out)
+			print(Height + "\t" + "\t" + Count + "\t" + Random)
+		elif data == bytearray(b'\x57\x00\xC2\x02'):
+			#out = ser.read(6)
+			#X, Y, Z = struct.unpack("<hhH", out)
+			#print (X, Y)
+			print("YYYY")
---
+++ plot_height_output.py
@@ -0,0 +1,8 @@
+import serial
+import struct
+import binascii
+import serial
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+
